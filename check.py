b55f46347876fb1e327d54eae7b093d935997f64
a-name-not-used <wangxin_0716@me.com>
2020-10-04 23:09:56 +0800

    x

--- check.py
+++ check.py
@@ -4,6 +4,7 @@ from PIL import Image
 import os
 from facenet_pytorch import MTCNN, extract_face,\
                             InceptionResnetV1, fixed_image_standardization
+from utils import compute_dist
 
 dir = 'logs/test'
 
@@ -20,8 +21,13 @@ for idx, line in enumerate(pair_in):
     src_adv = '{}_adv.png'.format(src[:4])
     print('src ', src_adv)
     print('tgt ', tgt)
-    src_img = Image.open(os.path.join('logs/test', src))  # adv
+    src_img = Image.open(os.path.join('logs/test', src_adv))  # adv
+    src_origin = Image.open(os.path.join('test', src))
+
+    dist = compute_dist(np.asarray(src_img), np.asarray(src_origin))
+    print('pixel dist {:.3f}'.format(dist))
     tgt_img = Image.open(os.path.join('test', tgt))
+
 
     src_cropped, _, _ = mtcnn(src_img)
     tgt_cropped, _, _ = mtcnn(tgt_img)
