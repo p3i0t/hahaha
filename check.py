a7233dd57a9f39b22df5057e67a4c9c77f70016a
a-name-not-used <wangxin_0716@me.com>
2020-10-04 23:13:57 +0800

    x

--- check.py
+++ check.py
@@ -30,19 +30,24 @@ for idx, line in enumerate(pair_in):
 
 
     src_cropped, _, _ = mtcnn(src_img)
+    src_origin_cropped, _, _ = mtcnn(src_origin)
     tgt_cropped, _, _ = mtcnn(tgt_img)
 
     src_rep = resnet1(src_cropped.unsqueeze(0))
+    src_origin_rep = resnet1(src_origin_cropped.unsqueeze(0))
     tgt_rep = resnet1(tgt_cropped.unsqueeze(0))
 
+    origin_sim1 = (src_origin_rep * tgt_rep).sum().item()
     sim1 = (src_rep * tgt_rep).sum().item()
-    print('similarity 1: {:.4f}'.format(sim1))
+    print('similarity 1, origin: {:.4f}, adv: {:.4f}'.format(origin_sim1, sim1))
 
     src_rep = resnet2(src_cropped.unsqueeze(0))
+    src_origin_rep = resnet2(src_origin_cropped.unsqueeze(0))
     tgt_rep = resnet2(tgt_cropped.unsqueeze(0))
 
+    origin_sim2 = (src_origin_rep * tgt_rep).sum().item()
     sim2 = (src_rep * tgt_rep).sum().item()
-    print('similarity 2: {:.4f}'.format(sim2))
+    print('similarity 2, origin: {:.4f}, adv: {:.4f}'.format(origin_sim2, sim2))
 
     if idx == 10:
         break
