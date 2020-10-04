db73528a1926cef3521b37b4e8361b983d3b666a
a-name-not-used <wangxin_0716@me.com>
2020-10-04 18:40:43 +0800

    x

--- main.py
+++ main.py
@@ -8,11 +8,46 @@ from PIL import Image
 import torch
 from torchvision.transforms import functional as F
 from torch.nn import functional as FF
+from torch.utils.data import Dataset, DataLoader
 from facenet_pytorch import MTCNN, extract_face,\
                             InceptionResnetV1, fixed_image_standardization
 
 from utils import crop_resize_back, compute_dist,\
                   tensor_to_image, array_to_image
+
+
+def preprocess_image(image_path):
+    "Load Image, normalize and convert to tensor."
+    img = Image.open(image_path)
+    img_tensor = F.to_tensor(np.float32(img))
+    return fixed_image_standardization(image_tensor=img_tensor)  # in [-1, 1]
+
+
+class PairDataset(Dataset):
+    def __init__(self, source_image_paths, target_image_paths):
+        super().__init__()
+        assert len(source_image_paths) == len(target_image_paths)
+        self.source_image_paths = source_image_paths
+        self.target_image_paths = target_image_paths
+
+    def __len__(self):
+        return len(self.source_image_paths)
+
+    def __getitem__(self, idx):
+        source = self.source_image_paths[idx]
+        target = self.target_image_paths[idx]
+        source_tensor = preprocess_image(source)
+        target_tensor = preprocess_image(target)
+        return source_tensor, target_tensor, source, target
+
+
+def customized_collate_fn(batch_list):
+    "A customized collate_fn function for Dataloader."
+    source = torch.cat([item[0].unsqueeze(0) for item in batch_list], dim=0)
+    target = torch.cat([item[1].unsqueeze(0) for item in batch_list], dim=0)
+    source_paths = [item[2].unsqueeze(0) for item in batch_list]
+    target_paths = [item[3].unsqueeze(0) for item in batch_list]
+    return source, target, source_paths, target_paths
 
 
 def fixed_image_standardization_inverse(image_tensor):
@@ -57,7 +92,7 @@ def cal_source_grad(inception_model, source_img, target_rep):
     source_img.requires_grad = True
     source_rep = inception_model(source_img)
 
-    similarity = (target_rep * source_rep).sum()
+    similarity = (target_rep * source_rep).sum(dim=1).mean()
 
     inception_model.zero_grad()
     # cal gradient
@@ -67,9 +102,7 @@ def cal_source_grad(inception_model, source_img, target_rep):
 
 
 def iterative_grad_attack(inception_model, source_img, target_img,
-                          n_steps=40, lr=0.01):
-    source_img = source_img.unsqueeze(0)
-    target_img = target_img.unsqueeze(0)
+                          n_steps=50, lr=0.01):
     with torch.no_grad():
         target_rep = inception_model(target_img).detach()
 
@@ -80,27 +113,22 @@ def iterative_grad_attack(inception_model, source_img, target_img,
         perturbed_img = perturbed_img + lr * grad
         perturbed_img = torch.clamp(perturbed_img, -1.0, 1.0).detach_()
         if step % 20 == 9:
-            print('step {}, loss: {:.4f}'.format(step, similarity.item()))
+            print('step {}, similarity: {:.4f}'.format(step, similarity.item()))
     adv_rep = inception_model(perturbed_img)
-    rep_dist = (target_rep * adv_rep).sum()
+    rep_dist = (target_rep * adv_rep).sum(dim=1).mean().cpu().item()
 
-    adv_img = tensor_to_image(fixed_image_standardization_inverse(perturbed_img.squeeze(0)))
-    target_img = tensor_to_image(fixed_image_standardization_inverse(target_img.squeeze(0)))
+    adv_imgs = [tensor_to_image(img.squeeze(0)) for img in fixed_image_standardization_inverse(perturbed_img).split(split_size=1, dim=0)]
+    target_imgs = [tensor_to_image(img.squeeze(0)) for img in fixed_image_standardization_inverse(target_img).split(split_size=1, dim=0)]
 
-    dist = compute_dist(np.asarray(adv_img), np.asarray(target_img))
-    return adv_img, dist, rep_dist
-
-
-def preprocess_image(image_path):
-    "Load Image, normalize and convert to tensor."
-    img = Image.open(image_path)
-    img_tensor = F.to_tensor(np.float32(img))
-    return fixed_image_standardization(image_tensor=img_tensor)
+    dist_list = [compute_dist(np.asarray(adv_img), np.asarray(target_img)) for (adv_img, target_img) in zip(adv_imgs, target_imgs)]
+    dist = np.mean(dist_list)
+    return adv_imgs, dist, rep_dist
 
 
 def attack(args, mode='val'):
     # Create an inception resnet (in eval mode):
     resnet = InceptionResnetV1(pretrained=args.dataset_pretrained).eval()
+    resnet = resnet.cuda()
 
     np.random.seed(args.seed)
     if mode == 'val':
@@ -114,38 +142,40 @@ def attack(args, mode='val'):
             os.mkdir(log_dir)
 
         pair_out = open(os.path.join(log_dir, 'pair.txt'), 'w')
-        for sample_idx, (source_path, target_path) in enumerate(zip(image_path_list_shuffle, image_path_list)):
-            if sample_idx % 20 == 19:
-                print('sample {} finished. '.format(sample_idx))
-            source_img = preprocess_image(source_path)
-            target_img = preprocess_image(target_path)
+        dataset = PairDataset(image_path_list_shuffle, image_path_list)
+        dataloader = DataLoader(dataset, batch_size=args.attack_batch_size,
+                                shuffle=False,
+                                drop_last=False, num_workers=8)
 
-            adv_img, dist, rep_dist = iterative_grad_attack(
+        for source_img, target_img, src_paths, tgt_paths in dataloader:
+            # source_img = preprocess_image(source_path)
+            # target_img = preprocess_image(target_path)
+            source_img = source_img.cuda()
+            target_img = target_img.cuda()
+
+            adv_imgs, dist, rep_dist = iterative_grad_attack(
                 resnet, source_img, target_img,
                 lr=args.attack_lr, n_steps=args.attack_steps)
 
             pixel_dist_list.append(dist)
             rep_dist_list.append(rep_dist)
-            source_id = source_path.split('/')[-1][:4]
-            target_id = target_path.split('/')[-1][:4]
-            source_name = '{}_adv.png'.format(source_id)
-            target_name = '{}.png'.format(target_id)
+            for idx, (source_path, target_path) in enumerate(zip(src_paths, tgt_paths)):
+                source_id = source_path.split('/')[-1][:4]
+                target_id = target_path.split('/')[-1][:4]
+                source_name = '{}_adv.png'.format(source_id)
+                target_name = '{}.png'.format(target_id)
 
-            # crop resize back and save.
-            origin_img = Image.open('val/{}.png'.format(source_id))
-            info_img = pickle.load(open('val_cropped/{}_info.pkl'.format(source_id), 'rb'))
+                # crop resize back and save.
+                origin_img = Image.open('val/{}.png'.format(source_id))
+                info_img = pickle.load(open('val_cropped/{}_info.pkl'.format(source_id), 'rb'))
 
-            recovered_img = crop_resize_back(origin_img, adv_img, info_img['box'], info_img['box_size'])
-            recovered_img.save(os.path.join(log_dir, source_name))
+                adv_img_full = crop_resize_back(origin_img, adv_imgs[idx], info_img['box'], info_img['box_size'])
+                adv_img_full.save(os.path.join(log_dir, source_name))
 
-            original_pixel_dist_list.append(compute_dist(np.asarray(recovered_img), np.asarray(origin_img)))
-            pair_out.write('{} {}\n'.format(source_name, target_name))
+                original_pixel_dist_list.append(compute_dist(np.asarray(adv_img_full), np.asarray(origin_img)))
+                pair_out.write('{} {}\n'.format(source_name, target_name))
         pair_out.close()
     else:
-        # image_path_list = glob.glob('test_cropped/*_cropped.png')
-        # image_path_list_shuffle = np.random.permutation(image_path_list)
-        if sample_idx % 20 == 19:
-            print('sample {} finished. '.format(sample_idx))
         pixel_dist_list = []
         rep_dist_list = []
         original_pixel_dist_list = []
@@ -154,46 +184,44 @@ def attack(args, mode='val'):
             os.mkdir(log_dir)
 
         pair_in = open('test/pair.txt', 'r')
-        for sample_idx, line in enumerate(pair_in):
-            source_name, target_name = line.strip().split(' ')
-            source_id = source_path[:4]
-            target_id = target_path[:4]
+        lines = pair_in.readlines()
+        src_paths = [line.strip().split(' ')[0] for line in lines]
+        src_paths = ['test_cropped/{}_cropped.png'.format(path[:4]) for path in src_paths]
 
-            source_path = os.path.join('test_cropped/{}_cropped.png'.format(source_id))
-            target_path = os.path.join('test_cropped/{}_cropped.png'.format(target_id))
-            source_img = preprocess_image(source_path)
-            target_img = preprocess_image(target_path)
+        tgt_paths = [line.strip().split(' ')[1] for line in lines]
+        tgt_paths = ['test_cropped/{}_cropped.png'.format(path[:4]) for path in tgt_paths]
 
-            adv_img, dist, rep_dist = iterative_grad_attack(
+        dataset = PairDataset(src_paths, tgt_paths)
+        dataloader = DataLoader(dataset, batch_size=args.attack_batch_size,
+                                shuffle=False,
+                                drop_last=False, num_workers=8)
+
+        for source_img, target_img, src_paths, tgt_paths in dataloader:
+            source_img = source_img.cuda()
+            target_img = target_img.cuda()
+
+            adv_imgs, dist, rep_dist = iterative_grad_attack(
                 resnet, source_img, target_img,
                 lr=args.attack_lr, n_steps=args.attack_steps)
 
-            # crop resize back and save.
-            origin_img = Image.open('test/{}.png'.format(source_id))
-            info_img = pickle.load(open('test_cropped/{}_info.pkl'.format(source_id), 'rb'))
-            recovered_img = crop_resize_back(origin_img, adv_img, info_img['box'], info_img['box_size'])
-            recovered_img.save(os.path.join(log_dir, '{}_adv.png'.format(source_id)))
-
             pixel_dist_list.append(dist)
             rep_dist_list.append(rep_dist)
-            original_pixel_dist_list.append(compute_dist(np.asarray(recovered_img), np.asarray(origin_img)))
-        pair_in.close()
+            for idx, (source_path, target_path) in enumerate(zip(src_paths, tgt_paths)):
+                source_id = source_path.split('/')[-1][:4]
+                target_id = target_path.split('/')[-1][:4]
+                source_name = '{}_adv.png'.format(source_id)
+                target_name = '{}.png'.format(target_id)
+
+                # crop resize back and save.
+                origin_img = Image.open('val/{}.png'.format(source_id))
+                info_img = pickle.load(open('test_cropped/{}_info.pkl'.format(source_id), 'rb'))
+
+                adv_img_full = crop_resize_back(origin_img, adv_imgs[idx], info_img['box'], info_img['box_size'])
+                adv_img_full.save(os.path.join(log_dir, source_name))
+
+                original_pixel_dist_list.append(compute_dist(np.asarray(adv_img_full), np.asarray(origin_img)))
+
     return np.mean(original_pixel_dist_list), np.mean(pixel_dist_list), np.mean(rep_dist_list)
-
-
-    # print('box size ', box_size)
-    # print(box)
-    # print(img_cropped.max(), img_cropped.min())
-    # img_out = resnet(img_cropped.unsqueeze(0))
-    # print(img_cropped.size())
-    # print(img_out.size())
-
-    # cropped_img = Image.open('img_cropped.png')
-    # print('crop size', cropped_img.size)
-    # img_recovered = crop_resize_back(img_origin, cropped_img, box, box_size)
-    # print('dist', compute_dist(np.asarray(img_origin), np.asarray(img_recovered)))
-    # img_recovered.save('recovered_img.png') 
-    # exit(0)
 
 
 if __name__ == "__main__":
@@ -217,8 +245,8 @@ if __name__ == "__main__":
                          pretrained: ['vggface2', 'casia-webface']")
 
     # Optimization hyperparams:
-    parser.add_argument("--n_batch_train", type=int,
-                        default=128, help="Minibatch size")
+    parser.add_argument("--attack_batch_size", type=int,
+                        default=20, help="attack batch size")
     parser.add_argument("--n_batch_test", type=int,
                         default=200, help="Minibatch size")
     parser.add_argument("--optimizer", type=str,
@@ -231,10 +259,6 @@ if __name__ == "__main__":
                         help="Base learning rate")
     parser.add_argument("--epochs", type=int, default=100,
                         help="Total number of training epochs")
-
-    # Inference hyperparams:
-    parser.add_argument("--percentile", type=float, default=0.01,
-                        help="percentile value for inference with rejection.")
 
     # Model hyperparams:
     parser.add_argument("--image_size", type=int,
